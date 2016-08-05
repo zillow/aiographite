825b285727c57420b5689d5c23df1bdf28d088e2
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2016-08-05 00:37:11 -0800

    adding documentation.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = []
 tests_require = []
 
 setup(name='aiographite',
-      version='0.0.1',
+      version='0.0.2',
       description='',
       long_description=open(README_PATH).read(),
       author='Yun Xu',
