1ec0ccd78c35799a5b3d2b6846719bc711412926
Yun Xu <yunx@yunx1.zillow.local>
2016-08-18 16:04:45 -0700

    add test for valid graphite metric name

---
+++ tests/test_consistency.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pytest
+from aiographite.graphite_escaping import metrics_name_to_graphite, metrics_name_from_graphite
+
+
+@pytest.mark.parametrize("name", [
+    'abc_edf',
+    'abc @edf#',
+    'abc.@edf#',
+    'abc_ @ e_df#',
+    'a.b.c_ @ e_df#',
+    'a.b.___c d _feg',
+    '_ . .fda',
+    '_.',
+    '汉 字.汉*字'
+])
+def test_consistency(name):
+	assert metrics_name_from_graphite(metrics_name_to_graphite(name)) == name
