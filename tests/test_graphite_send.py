792194b10e8da8ae401dbb78a14a589f22f1e50a
yunstanford <yunx@zillow.com>
2016-08-24 11:22:38 -0700

    coding style for flake8  3

--- tests/test_graphite_send.py
+++ tests/test_graphite_send.py
@@ -1,16 +1,14 @@
 import pytest
-from aiographite.aiographitesend import AioGraphiteSendException, AIOGraphite
+from aiographite.aiographitesend import AIOGraphite
 from aiographite.protocol import PlaintextProtocol, PickleProtocol
-import socket
-import unittest
 import asyncio
 from asyncio import test_utils
 
 DEFAULT_GRAPHITE_PLAINTEXT_PORT = 2003
 DEFAULT_GRAPHITE_PICKLE_PORT = 2004
 
-################ Unit Tests ##################
 
+# Unit Tests
 @pytest.mark.parametrize("metric, value, timestamp", [
     ('zillow', 124, 234),
     ('trulia', 223, 435),
@@ -18,7 +16,9 @@ DEFAULT_GRAPHITE_PICKLE_PORT = 2004
 ])
 def test_pickle_protocol_formatted_data(metric, value, timestamp):
     pickle = PickleProtocol()
-    assert pickle.data_format(metric, value, timestamp) == (metric, (timestamp, value))
+    data = pickle.data_format(metric, value, timestamp)
+    expected_data = (metric, (timestamp, value))
+    assert data == expected_data
 
 
 @pytest.mark.parametrize("metric, value, timestamp, expected_data", [
@@ -28,23 +28,27 @@ def test_pickle_protocol_formatted_data(metric, value, timestamp):
 ])
 def test_plaintext_protocol_formatted_data(metric, value, timestamp, expected_data):
     plaintext = PlaintextProtocol()
-    assert plaintext.data_format(metric, value, timestamp) == expected_data
+    data = plaintext.data_format(metric, value, timestamp)
+    assert data == expected_data
 
 
 def test_generate_message_for_pickle():
     pickle = PickleProtocol()
     tuple_list = [('a', (123, 456)), ('b', (123, 456)), ('c', (876, 987))]
-    expected_message = (b'\x00\x00\x00B\x80\x02]q\x00(X\x01\x00\x00\x00aq\x01'
-    b'K{M\xc8\x01\x86q\x02\x86q\x03X\x01\x00\x00\x00'
-    b'bq\x04K{M\xc8\x01\x86q\x05\x86q\x06X\x01\x00\x00\x00cq\x07Ml\x03M\xdb\x03\x86q\x08\x86q\te.')
+    expected_message = (
+        b'\x00\x00\x00B\x80\x02]q\x00(X\x01\x00\x00\x00aq\x01'
+        b'K{M\xc8\x01\x86q\x02\x86q\x03X\x01\x00\x00\x00'
+        b'bq\x04K{M\xc8\x01\x86q\x05\x86q\x06X\x01\x00\x00'
+        b'\x00cq\x07Ml\x03M\xdb\x03\x86q\x08\x86q\te.')
     assert pickle.generate_message(tuple_list) == expected_message
 
 
 def test_generate_message_for_plaintext():
-    plaintext_list = ["metric1 value1 timestamp1\n", 
-                    "metric2 value2 timestamp2\n", 
+    plaintext_list = ["metric1 value1 timestamp1\n",
+                    "metric2 value2 timestamp2\n",
                     "metric3 value3 timestamp3\n"]
-    expected_message = (b'metric1 value1 timestamp1\n'
+    expected_message = (
+        b'metric1 value1 timestamp1\n'
         b'metric2 value2 timestamp2\n'
         b'metric3 value3 timestamp3\n')
     plaintext = PlaintextProtocol()
@@ -53,17 +57,18 @@ def test_generate_message_for_plaintext():
 
 
 @pytest.mark.parametrize("metric_parts, expected_metric_name", [
-    (['sproc performance', 'velo@zillow.com', '::EH12'], 
-        'sproc%20performance.velo%40zillow%2Ecom.%3A%3AEH12'),
-    (['dit_400', 'zpid@zillow.com', 'EHT::disk_usage_per_host'], 
-        'dit_400.zpid%40zillow%2Ecom.EHT%3A%3Adisk_usage_per_host'),
+    (['sproc performance', 'velo@zillow.com', '::EH12'],
+      'sproc%20performance.velo%40zillow%2Ecom.%3A%3AEH12'),
+    (['dit_400', 'zpid@zillow.com', 'EHT::disk_usage_per_host'],
+      'dit_400.zpid%40zillow%2Ecom.EHT%3A%3Adisk_usage_per_host'),
 ])
 def test_clean_and_join_metric_parts(metric_parts, expected_metric_name):
     with test_utils.run_test_server() as httpd:
         loop = asyncio.get_event_loop()
         plaintext_protocol = PlaintextProtocol()
         aiographite = AIOGraphite(*httpd.address, plaintext_protocol, loop=loop)
-        assert aiographite.clean_and_join_metric_parts(metric_parts) == expected_metric_name
+        metric_name = aiographite.clean_and_join_metric_parts(metric_parts)
+        assert metric_name == expected_metric_name
 
 
 def test_generate_message_for_data_list(metric_value_timestamp_list, timestamp):
@@ -72,12 +77,13 @@ def test_generate_message_for_data_list(metric_value_timestamp_list, timestamp):
         plaintext_protocol = PlaintextProtocol()
         aiographite = AIOGraphite(*httpd.address, plaintext_protocol, loop=loop)
         message = aiographite._generate_message_for_data_list(
-            metric_value_timestamp_list, 
-            timestamp, 
-            plaintext_protocol.data_format, 
+            metric_value_timestamp_list,
+            timestamp,
+            plaintext_protocol.data_format,
             plaintext_protocol.generate_message
-            )
-        expected_message = (b'zillow 124 1471640958\ntrulia 223 1471640923\n'
+        )
+        expected_message = (
+            b'zillow 124 1471640958\ntrulia 223 1471640923\n'
             b'hotpad 53534 1471640943\nstreeteasy 13424 1471640989\n')
         assert message == expected_message
 
@@ -118,7 +124,7 @@ def test_send_message_and_send():
     
     async def test_send_message():
         plaintext_protocol = PlaintextProtocol()
-        aiographite = AIOGraphite('127.0.0.1', 
+        aiographite = AIOGraphite('127.0.0.1',
             DEFAULT_GRAPHITE_PLAINTEXT_PORT, plaintext_protocol, loop=loop)
         message = "hello world !"
         await aiographite.connect_to_graphite()
@@ -134,7 +140,7 @@ def test_send_message_and_send():
 
     async def test_send():
         plaintext_protocol = PlaintextProtocol()
-        aiographite = AIOGraphite('127.0.0.1', 
+        aiographite = AIOGraphite('127.0.0.1',
             DEFAULT_GRAPHITE_PLAINTEXT_PORT, plaintext_protocol, loop=loop)
         await aiographite.connect_to_graphite()
         metric = 'sproc%20performance.velo%40zillow%2Ecom.%3A%3AEH12'
@@ -155,9 +161,9 @@ def test_send_message_and_send():
         pickle = PickleProtocol()
         aiographite = AIOGraphite('127.0.0.1', DEFAULT_GRAPHITE_PLAINTEXT_PORT, pickle, loop=loop)
         await aiographite.connect_to_graphite() 
-        dataset = [('sproc%20performance.velo%40zillow%2Ecom.%3A%3AEH12', 
+        dataset = [('sproc%20performance.velo%40zillow%2Ecom.%3A%3AEH12',
                     3233, 1471640923),
-                    ('dit_400.zpid%40zillow%2Ecom.EHT%3A%3Adisk_usage_per_host', 
+                   ('dit_400.zpid%40zillow%2Ecom.EHT%3A%3Adisk_usage_per_host',
                     2343, 1471640976)]
         await aiographite.send_multiple(dataset)
         reader = aiographite._reader
@@ -167,10 +173,12 @@ def test_send_message_and_send():
         data = (await reader.read())
         writer.close()
         aiographite.disconnect()
-        message = (b"\x00\x00\x00\x9c\x80\x02]q\x00(X2\x00\x00\x00sproc"
+        message = (
+            b"\x00\x00\x00\x9c\x80\x02]q\x00(X2\x00\x00\x00sproc"
             b"%20performance.velo%40zillow%2Ecom.%3A%3AEH12q\x01J[u\xb7WM"
             b"\xa1\x0c\x86q\x02\x86q\x03X8\x00\x00\x00dit_400.zpid%40zillow"
-            b"%2Ecom.EHT%3A%3Adisk_usage_per_hostq\x04J\x90u\xb7WM'\t\x86q\x05\x86q\x06e.")
+            b"%2Ecom.EHT%3A%3Adisk_usage_per_hostq\x04J\x90u\xb7WM'\t\x86q"
+            b"\x05\x86q\x06e.")
         assert message == data
 
     def run(task, loop):
